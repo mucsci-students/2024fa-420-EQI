0511d10f875d008fac5cbc5fe4206f069ec6e32b
xhatd <isleo013@gmail.com>
2024-09-12 11:32:35 -0400

    Simple Format checking

---
+++ UML_UTILITY/FORMAT_CHECKING/validators.py
@@ -0,0 +1,37 @@
+"""
+Author : Israel Gonzalez
+Created: September 12, 2024
+Version: 1.0
+
+Description: 
+This function, check_format, validates whether a given string follows a specific format. 
+The checks include:
+1. The input must be a string.
+2. The input length must be between 2 and 50 characters.
+3. The input can only contain alphabetic characters (no spaces or special characters).
+4. The input must be all lowercase letters.
+"""
+
+import re
+
+VALID_NAME_PATTERN = re.compile(r"^[a-zA-Z]+$")
+
+def check_format(name):
+
+    # Check if the input is a string and matches the pattern
+    if not isinstance(name, str):
+        return "Input must be a string."
+    
+    # Check if minimum length 2, maximum length 50
+    if not (2 <= len(name) <= 50):  
+        return "Invalid length. Must be between 2 and 50 characters."
+    
+    # Check if the input matches the regex pattern
+    if not VALID_NAME_PATTERN.match(name):
+        return "Invalid format. Only alphabet characters are allowed."
+    
+    # Check if the string contains any spaces
+    if ' ' in name:
+        return "Invalid format. Spaces are not allowed."
+    
+    return "Valid input"
